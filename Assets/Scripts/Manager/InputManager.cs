40bc0868afbd9a08c92169834d3c49526ce9a1ef
Rick <rick1130_iphone@icloud.com>
2017-01-02 23:57:08 +0900

    switch platform iOS and add scripts

---
+++ Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : SingletonMonoBehaviour<InputManager> {
+
+	//とりあえず宣言しておく
+	public enum InputKey {
+		Left_Click,
+		Right_Click,
+		Wheel,
+		Horizontal,
+		Vertical,
+		Enter,
+		Space,
+		Shift,
+		Control,
+		Input_Num,
+	}
+
+	public delegate void InputDelegate();
+	public InputDelegate[] InputKeyDown;
+
+
+	protected override void Awake() {
+		base.Awake();
+
+		InputKeyDown = new InputDelegate[ (int) InputKey.Input_Num];
+		//Debug
+		InputKeyDown[(int)InputKey.Left_Click] = LeftClick;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown( KeyCode.Mouse0 )) {
+			InputKeyDown[(int)InputKey.Left_Click]();
+		}
+	}
+
+	//Debug
+	void LeftClick() {
+		Debug.Log("LeftClick!");
+	}
+}
