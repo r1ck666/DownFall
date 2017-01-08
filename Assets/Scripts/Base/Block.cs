1562f05d48e6b0e332c1e4008af8667180ab3b06
Rick <rick1130_iphone@icloud.com>
2017-01-08 17:34:15 +0900

    Add Block

---
+++ Assets/Scripts/Base/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockState {
+	NONE,
+	NORMAL,
+	UNBREAK,
+	BROKEN,
+}
+
+public class Block : MonoBehaviour {
+
+	[SerializeField] string name;
+	public string Name {
+		get { return name; }
+		set { name = value; }
+	}
+
+	[SerializeField] int durability;
+	public int Durability {
+		get { return durability; }
+		set { durability = value; }
+	}
+
+	[SerializeField] BlockState state;
+	public BlockState State {
+		get { return state; }
+		set { state = value; }
+	}
+}
